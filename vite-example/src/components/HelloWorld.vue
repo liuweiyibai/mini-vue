8587546989f5065b63d1380be7a54ddc2c8c7d45
威 刘 <lw1140@163.com>
2022-06-06 15:18:18 +0800

    feat: 增加vue demo

---
+++ vite-example/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, reactive } from 'vue';
+
+defineProps({
+  msg: String,
+});
+
+const obj = ref({});
+const obj2 = reactive({});
+
+function addObj() {
+  const key = +new Date();
+  obj.value[key] = key;
+}
+function addObj2() {
+  const key = +new Date();
+  obj2[key] = key;
+}
+</script>
+
+<template>
+  <!-- 循环obj -->
+  <li v-for="item in obj" :key="item.id">{{ item }}</li>
+  <button @click="addObj">给obj增加属性</button>
+
+  <li v-for="item in obj2" :key="item.id">{{ item }}</li>
+  <button @click="addObj2">给obj2增加属性</button>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
